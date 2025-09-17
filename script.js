18f35870c668d92bf53fb29316b9a46dac3fae6c
Mariana Islas <marianaislasm2004@gmail.com>
2025-09-16 18:11:52 -0700

    Feat: Phone Responsiveness

--- script.js
+++ script.js
@@ -10,17 +10,35 @@ function currentSlide(n) {
 }
 
 function showSlides(n) {
-    let i;
-    let slides = document.getElementsByClassName("slide")
-    let dots = document.getElementsByClassName("dot");
-    if (n > slides.length) {slideIndex = 1}
-    if (n < 1) {slideIndex = slides.length}
-    for (i = 0; i < slides.length; i++) {
-      slides[i].style.display = "none";
+    let container, slides, dots;
+
+    if (document.querySelector(".desktop-nav").offsetParent !== null) {
+        container = document.querySelector(".desktop-nav");
+        slides = container.getElementsByClassName("slide-desktop");
+        dots = container.getElementsByClassName("dot-desktop");
+    } else {
+        container = document.querySelector(".phone-nav");
+        slides = container.getElementsByClassName("slide-phone");
+        dots = container.getElementsByClassName("dot-phone");
     }
-    for (i = 0; i < dots.length; i++) {
-      dots[i].className = dots[i].className.replace(" active", "");
+
+    if (n > slides.length) { slideIndex = 1; }
+    if (n < 1) { slideIndex = slides.length; }
+
+    for (let i = 0; i < slides.length; i++) {
+        slides[i].style.display = "none";
     }
-    slides[slideIndex-1].style.display = "block";
-    dots[slideIndex-1].className += " active";
+    for (let i = 0; i < dots.length; i++) {
+        dots[i].className = dots[i].className.replace(" active", "");
+    }
+
+    slides[slideIndex - 1].style.display = "block";
+    dots[slideIndex - 1].className += " active";
+
+    if (container) {
+        container.querySelector(".slideshow-container").style.minHeight = slides[0].offsetHeight + "px";
+    }
+
+    window.plusSlides = plusSlides;
+    window.currentSlide = currentSlide;
 }

f5305c709154a991d7e6e2a58bd0b38b12e3a144
Mariana Islas <marianaislasm2004@gmail.com>
2025-11-14 00:21:54 -0700

    Feat: Fixed image

--- portfolio/src/components/AboutPage.tsx
+++ portfolio/src/components/AboutPage.tsx
@@ -7,9 +7,9 @@ const AboutPage = () => {
                 <div className='sinoreta text-7xl text-[#FF0080] mb-10'>About Me</div>
                 <div className='assistant text-3xl text-justify'>Hello! I am Mariana Islas a designer that is passionate about creating visual experiences that communicate with intention. <br/><br/> 
 
-                I have participated in educational, cultural, and digital projects, where design stands out for its aesthetics, clarity, and ease of use. <br/><br/>
+                I have participated in educational, cultural, and digital projects, where design stands out for its aesthetics, clarity, and ease of use.<br/><br/>
 
-                I love combining creativity with logic, always striving for each project to create a genuine connection with people. </div>
+                I love combining creativity with logic, always striving for each project to create a genuine connection with people.</div>
             </div>
             <div className='w-[50%] flex justify-center'>
                 <img className="w-[70%] shadow-xl rounded-[20%]" src={Myself}/>
